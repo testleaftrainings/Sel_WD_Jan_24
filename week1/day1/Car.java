3aa112d0a90114f0a6dc9751701895723efd8232
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-01-25 13:26:42 +0530

    Add files via upload

---
+++ week1/day1/Car.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Car {
+	
+	
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("Hello Welcome to Testleaf");
+	}
+	
+	   
+}
+
